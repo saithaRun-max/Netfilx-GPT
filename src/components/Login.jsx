70b448ff878a419b8c76851e64f4f8cf0702046e
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-02-20 21:14:43 +0530

    validation&firebase setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,59 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [validStatus, setVlidStatus] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+  const firstName = useRef(null);
+  const mobileNumber = useRef(null);
+
+  const handleButtonClick = () => {
+    setVlidStatus(checkValidData(email.current.value, password.current.value ));
+  };
 
   const handleToggleSignIn = () => {
     setIsSignInForm(!isSignInForm);
-
-    // console.log(isSignUpForm);
   };
 
   return (
     <div>
       <Header />
-      <div className="fixed">
+      <div className=" ">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           alt="bg-image"
-          className=""
+          className="h-full w-full"
         />
       </div>
 
-      <div className="relative top-20  bg-black opacity-80 w-[420px] h-[600px] p-2  mx-auto ">
+      <div className="absolute top-24 left-1/3 bg-black opacity-80 w-[420px] h-[580px] p-2  mx-auto ">
         <h1 className="font-semibold text-white text-4xl px-1 mt-10 ml-8 ">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
-        <form className=" flex flex-col mt-10 px-10">
-          {!isSignInForm ? <div className="">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault(), handleButtonClick();
+          }}
+          className=" flex flex-col mt-10 px-10"
+        >
+          {!isSignInForm ? (
+            <div className="">
               <input
-              className=" bg-slate-500 placeholder:text-indigo-100 text-white font-medium w-full  p-3 mb-5  rounded-sm"
-              type="text"
-              placeholder="Enter Full Name"
-            />
-            
-            <input
-            className=" bg-slate-500 placeholder:text-indigo-100 text-white font-medium w-full p-3 mb-5  rounded-sm"
-            type="text"
-            placeholder="Enter Phone Number" /> 
-            
-            
-            </div> : ""
-        }
+                className=" bg-slate-500 placeholder:text-indigo-100 text-white font-medium w-full  p-3 mb-5  rounded-sm"
+                type="text"
+                ref={fullName}
+                placeholder="Enter Full Name"
+              />
+
+              <input
+                className=" bg-slate-500 placeholder:text-indigo-100 text-white font-medium w-full p-3 mb-5  rounded-sm"
+                type="text"
+                ref={phoneNumber}
+                placeholder="Enter Phone Number"
+              />
+            </div>
+          ) : (
+            ""
+          )}
 
           <input
             className=" bg-slate-500 placeholder:text-indigo-100  font-medium p-3 mb-5  rounded-sm"
             type="text"
+            ref={email}
             placeholder="Email Address"
           />
           <input
             className=" bg-slate-500 placeholder:text-indigo-100 font-medium p-3 mb-5 rounded-sm"
             type="password"
+            ref={password}
             placeholder="Password"
           />
+          <p className="text-white mx-5  cursor-pointer">{validStatus}</p>
+
           <button className="bg-red-600 p-3 text-white w-full rounded-sm mt-5 mx-auto  font-bold">
-          {isSignInForm ? "Sign In" : "Sign Up"}
+            {isSignInForm ? "Sign In" : "Sign Up"}
           </button>
         </form>
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_BBvxC_0EboO_PNecmDTokbGRchIoFzU",
+  authDomain: "netfilxgpt-001.firebaseapp.com",
+  projectId: "netfilxgpt-001",
+  storageBucket: "netfilxgpt-001.appspot.com",
+  messagingSenderId: "721399260427",
+  appId: "1:721399260427:web:a512fcf27cc720d16a6e0e",
+  measurementId: "G-9VPT9KLEHF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// export const auth = 
